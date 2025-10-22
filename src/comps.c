1f4cc4acb5d1adf15da91cedf3259ae558b7a019
Bella Campbell <Bella.Campbell@tuta.io>
2025-10-22 21:30:03 +0100

    Write c code

---
+++ src/comps.c
@@ -0,0 +1,404 @@
+COMP(
+  "Peterborough Open 2018",
+  "https://www.worldcubeassociation.org/competitions/PeterboroughOpen2018",
+  "My first comp"
+);
+COMP(
+  "Manchester Open 2019",
+  "https://www.worldcubeassociation.org/competitions/ManchesterOpen2019",
+  "My first comp in Manchester"
+);
+COMP(
+  "Lincoln Open 2019",
+  "https://www.worldcubeassociation.org/competitions/LincolnOpen2019",
+  "My first comp in Lincoln"
+);
+COMP(
+  "Leeds Open 2019",
+  "https://www.worldcubeassociation.org/competitions/LeedsOpen2019",
+  "My first comp in Leeds"
+);
+COMP(
+  "UK Championship 2019",
+  "https://www.worldcubeassociation.org/competitions/UKChampionship2019",
+  "My first UKC"
+);
+COMP(
+  "Doncaster Open 2020",
+  "https://www.worldcubeassociation.org/competitions/DoncasterOpen2020",
+  "My first comp in Doncaster"
+);
+COMP(
+  "Manchester Open 2020",
+  "https://www.worldcubeassociation.org/competitions/ManchesterOpen2020",
+  "My last pre-covid comp"
+);
+COMP(
+  "Wakefield Open 2022",
+  "https://www.worldcubeassociation.org/competitions/WakefieldOpen2022",
+  "My first post-covid comp"
+);
+COMP(
+  "Farnworth Spring C 2022",
+  "https://www.worldcubeassociation.org/competitions/FarnworthSpringC2022",
+  "My first comp in Farnworth"
+);
+COMP(
+  "Glasgow Open 2022",
+  "https://www.worldcubeassociation.org/competitions/GlasgowOpen2022",
+  "My first comp in Glasgow"
+);
+COMP(
+  "Carlisle Summer 2022",
+  "https://www.worldcubeassociation.org/competitions/CarlisleSummer2022",
+  "My first comp in Carlisle"
+);
+COMP(
+  "Lincoln Summer 2022",
+  "https://www.worldcubeassociation.org/competitions/LincolnSummer2022"
+);
+COMP(
+  "Kewbz UK Championship 2022",
+  "https://www.worldcubeassociation.org/competitions/KewbzUKChampionship2022",
+  "My first comp in Coventry"
+);
+COMP(
+  "Glasgow Winter 2023",
+  "https://www.worldcubeassociation.org/competitions/GlasgowWinter2023"
+);
+COMP(
+  "Edinburgh Spring 2023",
+  "https://www.worldcubeassociation.org/competitions/EdinburghSpring2023",
+  "My first comp in Edinburgh"
+);
+COMP(
+  "Sheffield Spring - BBO 2023",
+  "https://www.worldcubeassociation.org/competitions/SheffieldSpringBBO2023",
+  "My first comp in Sheffield and my first Brittish Blind Off"
+);
+COMP(
+  "Glasgow Spring 2023",
+  "https://www.worldcubeassociation.org/competitions/GlasgowSpring2023"
+);
+COMP(
+  "Edinburgh Summer 2023",
+  "https://www.worldcubeassociation.org/competitions/EdinburghSummer2023"
+);
+COMP(
+  "North Highlands Summer 2023",
+  "https://www.worldcubeassociation.org/competitions/NorthHighlandsSummer2023",
+  "My first comp in Wick and my first time getting on the podium... for 6 events!"
+);
+COMP(
+  "ABHM 2023",
+  "https://www.worldcubeassociation.org/competitions/ABHM2023",
+  "My first August Bank Holiday (Manchester)"
+);
+COMP(
+  "Glasgow Autumn 2023",
+  "https://www.worldcubeassociation.org/competitions/GlasgowAutumn2023",
+  "Got my first official 3BLD success"
+);
+COMP(
+  "Rubik's UK Championship FMC 2023",
+  "https://www.worldcubeassociation.org/competitions/RubiksUKChampionshipFMC2023",
+  "My first UKC FMC"
+);
+COMP(
+  "Rubik's UK Championship 2023",
+  "https://www.worldcubeassociation.org/competitions/RubiksUKChampionship2023"
+);
+COMP(
+  "Aberdeen Autumn 2023",
+  "https://www.worldcubeassociation.org/competitions/AberdeenAutumn2023",
+  "My first comp in Aberdeen"
+);
+COMP(
+  "Liverpool Winter 2023",
+  "https://www.worldcubeassociation.org/competitions/LiverpoolWinter2023",
+  "My first comp in Liverpool"
+);
+COMP(
+  "Edinburgh Winter 2024",
+  "https://www.worldcubeassociation.org/competitions/EdinburghWinter2024"
+);
+COMP(
+  "Glasgow Winter - SBO 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowWinterSBO2024",
+  "My first Scottish Blind Off"
+);
+COMP(
+  "Glasgow Spring 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowSpring2024"
+);
+COMP(
+  "Manchester MBLD Day 2024",
+  "https://www.worldcubeassociation.org/competitions/ManchesterMBLDDay2024",
+  "My first Manchester `Event` Day"
+);
+COMP(
+  "Manchester Spring 2024",
+  "https://www.worldcubeassociation.org/competitions/ManchesterSpring2024"
+);
+COMP(
+  "Bolton Spring 2024",
+  "https://www.worldcubeassociation.org/competitions/BoltonSpring2024",
+  "My first comp in Bolton"
+);
+COMP(
+  "Crewe Spring 2024",
+  "https://www.worldcubeassociation.org/competitions/CreweSpring2024",
+  "My first comp in Crewe"
+);
+COMP(
+  "Newcastle Tyneside Spring 2024",
+  "https://www.worldcubeassociation.org/competitions/NewcastleTynesideSpring2024",
+  "My first comp in Newcastle"
+);
+COMP(
+  "FMC 2024",
+  "https://www.worldcubeassociation.org/competitions/FMC2024",
+  "My first FMC sub-30 Single and Mean"
+);
+COMP(
+  "Glasgow Summer - SBO 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowSummerSBO2024",
+  "Got my first official 4BLD success"
+);
+COMP(
+  "Wakefield Summer 2024",
+  "https://www.worldcubeassociation.org/competitions/WakefieldSummer2024"
+);
+COMP(
+  "Edinburgh Summer Open 2024",
+  "https://www.worldcubeassociation.org/competitions/EdinburghSummerOpen2024"
+);
+COMP(
+  "Bolton Summer 2024",
+  "https://www.worldcubeassociation.org/competitions/BoltonSummer2024",
+  "Got my second official 4BLD success"
+);
+COMP(
+  "Stevenage July 2024",
+  "https://www.worldcubeassociation.org/competitions/StevenageJuly2024"
+);
+COMP(
+  "WCA European Championship 2024",
+  "https://www.worldcubeassociation.org/competitions/Euro2024",
+  "My first Continental Championship and my first comp outside of the UK!"
+);
+COMP(
+  "Stevenage August 2024",
+  "https://www.worldcubeassociation.org/competitions/StevenageAugust2024"
+);
+COMP(
+  "Birmingham Summer 2024",
+  "https://www.worldcubeassociation.org/competitions/BirminghamSummer2024",
+  "My first comp in Birmingham"
+);
+COMP(
+  "Kewbz UK Open 2024",
+  "https://www.worldcubeassociation.org/competitions/KewbzUKOpen2024",
+  "My first comp in London"
+);
+COMP(
+  "Glasgow Side Events Autumn 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowSideEventsAutumn2024"
+);
+COMP(
+  "Glasgow Fav Evening I 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowFavEveningI2024"
+);
+COMP(
+  "Stevenage October 2024",
+  "https://www.worldcubeassociation.org/competitions/StevenageOctober2024"
+);
+COMP(
+  "Glasgow 3x3 Evening 2024",
+  "https://www.worldcubeassociation.org/competitions/Glasgow3x3Evening2024"
+);
+COMP(
+  "Glasgow Autumn - SBO 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowAutumnSBO2024"
+);
+COMP(
+  "Cambridge Autumn BBO 2024",
+  "https://www.worldcubeassociation.org/competitions/CambridgeAutumnBBO2024",
+  "My first comp in Cambridge"
+);
+COMP(
+  "Glasgow Fav Evening II 2024",
+  "https://www.worldcubeassociation.org/competitions/GlasgowFavEveningII2024"
+);
+COMP(
+  "Rubik's UK Championship FMC 2024",
+  "https://www.worldcubeassociation.org/competitions/RubiksUKChampionshipFMC2024"
+);
+COMP(
+  "Rubik's UK Championship 2024",
+  "https://www.worldcubeassociation.org/competitions/RubiksUKChampionship2024"
+);
+COMP(
+  "FMC Europe 2024",
+  "https://www.worldcubeassociation.org/competitions/FMCEurope2024"
+);
+COMP(
+  "Wiltshire November 2024",
+  "https://www.worldcubeassociation.org/competitions/WiltshireNovember2024",
+  "My first comp in Wiltshire"
+);
+COMP(
+  "Manchester FMC Day 2024",
+  "https://www.worldcubeassociation.org/competitions/ManchesterFMCDay2024"
+);
+COMP(
+  "Stevenage December 2024",
+  "https://www.worldcubeassociation.org/competitions/StevenageDecember2024"
+);
+COMP(
+  "Norwich Winter 2024",
+  "https://www.worldcubeassociation.org/competitions/NorwichWinter2024",
+  "My first comp in Norwich"
+);
+COMP(
+  "Crewe Favourites 2025",
+  "https://www.worldcubeassociation.org/competitions/CreweFavourites2025"
+);
+COMP(
+  "Crewe FMC Day 2025",
+  "https://www.worldcubeassociation.org/competitions/CreweFMCDay2025"
+);
+COMP(
+  "Chichester January 2025",
+  "https://www.worldcubeassociation.org/competitions/ChichesterJanuary2025",
+  "My first comp in Chicester"
+);
+COMP(
+  "Crewe Winter 2025",
+  "https://www.worldcubeassociation.org/competitions/CreweWinter2025"
+);
+COMP(
+  "Hampshire Winter 2025",
+  "https://www.worldcubeassociation.org/competitions/HampshireWinter2025",
+  "My first comp in Hampshire"
+);
+COMP(
+  "Glasgow Spring - SBO 2025",
+  "https://www.worldcubeassociation.org/competitions/GlasgowSpringSBO2025"
+);
+COMP(
+  "Leeds Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/LeedsSpring2025"
+);
+COMP(
+  "Glasgow Fav Evening III 2025",
+  "https://www.worldcubeassociation.org/competitions/GlasgowFavEveningIII2025"
+);
+COMP(
+  "Manchester 7x7 Day 2025",
+  "https://www.worldcubeassociation.org/competitions/Manchester7x7Day2025"
+);
+COMP(
+  "Glasgow 3x3 Evening March 2025",
+  "https://www.worldcubeassociation.org/competitions/Glasgow3x3EveningMarch2025"
+);
+COMP(
+  "Glasgow Side Events Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/GlasgowSideEventsSpring2025"
+);
+COMP(
+  "Birmingham Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/BirminghamSpring2025"
+);
+COMP(
+  "Manchester Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/ManchesterSpring2025"
+);
+COMP(
+  "Chichester April 2025",
+  "https://www.worldcubeassociation.org/competitions/ChichesterApril2025"
+);
+COMP(
+  "FMC Cheltenham Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/FMCCheltenhamSpring2025",
+  "My first comp in Cheltenham"
+);
+COMP(
+  "Crewe Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/CreweSpring2025"
+);
+COMP(
+  "Weston-super-Mare Spring 2025",
+  "https://www.worldcubeassociation.org/competitions/WestonsuperMareSpring2025",
+  "My first comp in Weston-super-Mare"
+);
+COMP(
+  "Scottish Championship Blind 2025",
+  "https://www.worldcubeassociation.org/competitions/ScottishChampionshipBlindOff2025",
+  "My first Scottish Blind Championship"
+);
+COMP(
+  "Kewbz Scottish Championship 2025",
+  "https://www.worldcubeassociation.org/competitions/KewbzScottishChampionship2025",
+  "My first Scottish Championship"
+);
+COMP(
+  "North Highlands Summer 2025",
+  "https://www.worldcubeassociation.org/competitions/NorthHighlandsSummer2025"
+);
+COMP(
+  "Manchester June 2025",
+  "https://www.worldcubeassociation.org/competitions/ManchesterJune2025"
+);
+COMP(
+  "Bristol June 2025",
+  "https://www.worldcubeassociation.org/competitions/BristolJune2025",
+  "My first comp in Bristol"
+);
+COMP(
+  "Newcastle upon Tyne Summer 2025",
+  "https://www.worldcubeassociation.org/competitions/NewcastleuponTyneSummer2025"
+);
+COMP(
+  "Glasgow NxN Day 2025",
+  "https://www.worldcubeassociation.org/competitions/GlasgowNxNDay2025"
+);
+COMP(
+  "Rubik's UK Championship FMC 2025",
+  "https://www.worldcubeassociation.org/competitions/RubiksUKChampionshipFMC2025"
+);
+COMP(
+  "Rubik's UK Championship 2025",
+  "https://www.worldcubeassociation.org/competitions/RubiksUKChampionship2025"
+);
+COMP(
+  "Bristol August 2025",
+  "https://www.worldcubeassociation.org/competitions/BristolAugust2025"
+);
+COMP(
+  "Manchester 6x6 Day 2025",
+  "https://www.worldcubeassociation.org/competitions/Manchester6x6Day2025"
+);
+COMP(
+  "Weston-super-Mare Autumn 2025",
+  "https://www.worldcubeassociation.org/competitions/WestonsuperMareAutumn2025"
+);
+COMP(
+  "Edinburgh Autumn 2025",
+  "https://www.worldcubeassociation.org/competitions/EdinburghAutumn2025"
+);
+COMP(
+  "Manchester Favourites I 2025",
+  "https://www.worldcubeassociation.org/competitions/ManchesterFavouritesI2025",
+  "I got my first ever 5x5 win :3 :3 :3"
+);
+COMP(
+  "Stevenage October 2025",
+  "https://www.worldcubeassociation.org/competitions/StevenageOctober2025",
+  "Got my third official 4BLD success"
+);
+COMP(
+  "Manchester Clock Day 2025",
+  "https://www.worldcubeassociation.org/competitions/ManchesterClockDay2025",
+  "The unnoficial event clock doubles was surprisingly fun :3"
+);
---
+++ src/gen.c
@@ -0,0 +1,397 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define OUTPUT_DIR "../docs"
+
+enum {
+  e_2x2,
+  e_3x3,
+  e_4x4,
+  e_5x5,
+  e_6x6,
+  e_7x7,
+  e_3BLD,
+  e_4BLD,
+  e_5BLD,
+  e_MBLD,
+  e_OH,
+  e_SQ1,
+  e_Pyra,
+  e_Skewb,
+  e_Mega,
+  e_Clock,
+  e_FMC,
+  EVENT_COUNT
+};
+
+const char *e_names[EVENT_COUNT] = {
+  [e_2x2] = "2x2",
+  [e_3x3] = "3x3",
+  [e_4x4] = "4x4",
+  [e_5x5] = "5x5",
+  [e_6x6] = "6x6",
+  [e_7x7] = "7x7",
+  [e_3BLD] = "3x3 Blindfolded",
+  [e_4BLD] = "4x4 Blindfolded",
+  [e_5BLD] = "5x5 Blindfolded",
+  [e_MBLD] = "3x3 Multi-Blind",
+  [e_OH] = "3x3 One Handed",
+  [e_SQ1] = "Square-1",
+  [e_Pyra] = "Pyraminx",
+  [e_Skewb] = "Skewb",
+  [e_Mega] = "Megaminx",
+  [e_Clock] = "Clock",
+  [e_FMC] = "3x3 Fewest Moves",
+};
+
+struct {
+  const char *single;
+  const char *average;
+} pbs[EVENT_COUNT];
+
+typedef struct {
+  const char *name;
+  const char *url;
+  const char *text;
+} comp;
+
+// https://gist.github.com/rexim/b5b0c38f53157037923e7cdd77ce685d
+#define da_append(xs, x)                                                       \
+  do {                                                                         \
+    if ((xs)->count >= (xs)->capacity) {                                       \
+      if ((xs)->capacity == 0) (xs)->capacity = 256;                           \
+      else (xs)->capacity *= 2;                                                \
+      (xs)->items = realloc((xs)->items, (xs)->capacity*sizeof(*(xs)->items)); \
+    }                                                                          \
+    (xs)->items[(xs)->count++] = (x);                                          \
+  } while (0)
+
+struct {
+  size_t count;
+  size_t capacity;
+  comp *items;
+} comps;
+
+#define PB(event, format, pb) pbs[event].format = pb;
+
+#define COMP2(NAME, URL) da_append(&comps, ((comp){ .name = NAME, .url = URL, .text = NULL }));
+#define COMP3(NAME, URL, TEXT) da_append(&comps, ((comp){ .name = NAME, .url = URL, .text = TEXT }));
+
+#define GET_COMP(_1, _2, _3, NAME, ...) NAME
+#define COMP(...) GET_COMP(__VA_ARGS__, COMP3, COMP2)(__VA_ARGS__)
+
+void start_html(FILE *fp) {
+  fprintf(
+    fp,
+    "<!DOCTYPE html>\n"
+    "<html lang=\"en\">\n"
+    "  <head>\n"
+    "    <title>cubing pbs - RaspBella</title>\n"
+    "    <link rel=\"stylesheet\" href=\"/main.css\">\n"
+    "    <meta charset=\"UTF-8\">\n"
+    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+    "    <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/favicon/apple-touch-icon.png\">\n"
+    "    <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon/favicon-32x32.png\">\n"
+    "    <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon/favicon-16x16.png\">\n"
+    "    <link rel=\"manifest\" href=\"/favicon/site.webmanifest\">\n"
+    "  </head>\n"
+    "  <body>\n"
+    "    <ul class=\"nav\">\n"
+    "      <li><a href=\"/\">home</a></li>\n"
+    "      <li><a href=\"/RaspBella\">about</a></li>\n"
+    "      <li><a class=\"active\" href=\"/cubing\">cubing</a></li>\n"
+    "      <li><a href=\"/linux\">linux</a></li>\n"
+    "      <li><a href=\"/media\">media</a></li>\n"
+    "      <li><a href=\"/trans\">transportation</a></li>\n"
+    "    </ul>\n"
+  );
+}
+
+void end_html(FILE *fp) {
+  fprintf(
+    fp,
+    "  </body>\n"
+    "</html>"
+  );
+}
+
+void mbld2html(FILE *fp, const char *pb) {
+  fprintf(
+    fp,
+    "        <tr>\n"
+    "          <td>%s</td>\n"
+    "          <td colspan=\"2\">%s</td>\n"
+    "        </tr>\n",
+    e_names[e_MBLD],
+    pb
+  );
+}
+
+void other_event2html(FILE *fp, const char *event, const char *single, const char *average) {
+  fprintf(
+    fp,
+    "        <tr>\n"
+    "          <td>%s</td>\n"
+    "          <td>%s</td>\n"
+    "          <td>%s</td>\n"
+    "        </tr>\n",
+    event,
+    single,
+    average
+  );
+}
+
+void pbs2html(FILE *fp) {
+  start_html(fp);
+
+  fprintf(
+    fp,
+    "    <div>\n"
+    "      <h2>Cubing PBs</h2>\n"
+    "      <table>\n"
+    "        <tr>\n"
+    "          <th>Event</th>\n"
+    "          <th>PB Single</th>\n"
+    "          <th>PB Average <a href=\"/cubing/pbs#average\">(1)<a/></th>\n"
+    "        </tr>\n"
+  );
+
+  for (int i = 0; i < e_MBLD; i++) {
+    other_event2html(fp, e_names[i], pbs[i].single, pbs[i].average);
+  }
+
+  mbld2html(fp, pbs[e_MBLD].single);
+
+  for (int i = e_MBLD + 1; i < EVENT_COUNT; i++) {
+    other_event2html(fp, e_names[i], pbs[i].single, pbs[i].average);
+  }
+
+  fprintf(
+    fp,
+    "    </table>\n"
+    "    <a id=\"average\"><p style=\"margin: 1em;\" id=\"average\">Multi-Blind is the only event with no average/mean format</p></a>\n"
+    "    </div>\n"
+  );
+
+  end_html(fp);
+}
+
+void mbld2json(FILE *fp, const char *pb) {
+  fprintf(
+    fp,
+    "  \"%s\": \"%s\"",
+    e_names[e_MBLD],
+    pb
+  );
+}
+
+void other_event2json(FILE *fp, const char *event, const char *single, const char *average) {
+  fprintf(
+    fp,
+    "  \"%s\": {\n"
+    "    \"single\": \"%s\",\n"
+    "    \"average\": \"%s\"\n"
+    "  }",
+    event,
+    single,
+    average
+  );
+}
+
+void pbs2json(FILE *fp) {
+  fprintf(
+    fp,
+    "{\n"
+  );
+
+  other_event2json(fp, e_names[0], pbs[0].single, pbs[0].average);
+
+  for (int i = 1; i < e_MBLD; i++) {
+    fprintf(fp, ",\n");
+
+    other_event2json(fp, e_names[i], pbs[i].single, pbs[i].average);
+  }
+
+  fprintf(fp, ",\n");
+
+  mbld2json(fp, pbs[e_MBLD].single);
+
+  for (int i = e_MBLD + 1; i < EVENT_COUNT - 1; i++) {
+    fprintf(fp, ",\n");
+
+    other_event2json(fp, e_names[i], pbs[i].single, pbs[i].average);
+  }
+
+  fprintf(
+    fp,
+    "\n}"
+  );
+}
+
+void comp2html(FILE *fp, size_t n, comp comp) {
+  if (comp.text) {
+    fprintf(
+      fp,
+      "        <tr>\n"
+      "          <td>%zu</td>\n"
+      "          <td><a href=\"%s\">%s</a></td>\n"
+      "          <td>%s</td>\n"
+      "        </tr>\n",
+      n,
+      comp.url, comp.name,
+      comp.text
+    );
+  }
+
+  else {
+    fprintf(
+      fp,
+      "        <tr>\n"
+      "          <td>%zu</td>\n"
+      "          <td colspan=\"2\"><a href=\"%s\">%s</a></td>\n"
+      "        </tr>\n",
+      n,
+      comp.url, comp.name
+    );
+  }
+}
+
+void comps2html(FILE *fp) {
+  start_html(fp);
+
+  fprintf(
+    fp,
+    "    <div>\n"
+    "      <h2>My Comps</h2>\n"
+    "      <table>\n"
+    "        <tr>\n"
+    "          <th>#</th>\n"
+    "          <th>Name</th>\n"
+    "          <th>Info</th>\n"
+    "        </tr>\n"
+  );
+
+  size_t count = 0;
+  size_t year_count = 0;
+
+  char y[4] = {
+    [0] = comps.items->name[strlen(comps.items->name) - 4],
+    [1] = comps.items->name[strlen(comps.items->name) - 3],
+    [2] = comps.items->name[strlen(comps.items->name) - 2],
+    [3] = comps.items->name[strlen(comps.items->name) - 1]
+  };
+
+  for (size_t i = 0; i < comps.count; i++) {
+    char ny[4] = {
+      [0] = comps.items[i].name[strlen(comps.items[i].name) - 4],
+      [1] = comps.items[i].name[strlen(comps.items[i].name) - 3],
+      [2] = comps.items[i].name[strlen(comps.items[i].name) - 2],
+      [3] = comps.items[i].name[strlen(comps.items[i].name) - 1]
+    };
+
+    if (ny[3] != y[3] || ny[2] != y[2] || ny[1] != y[1] || ny[0] != y[0]) {
+      fprintf(
+        fp,
+        "        <tr>\n"
+        "          <td colspan=\"3\">%c%c%c%c: %zu/%zu</td>\n"
+        "        </tr>\n",
+        y[0], y[1], y[2], y[3], year_count, count
+      );
+
+      y[0] = ny[0];
+      y[1] = ny[1];
+      y[2] = ny[2];
+      y[3] = ny[3];
+
+      year_count = 0;
+    }
+
+    comp2html(fp, i + 1, comps.items[i]);
+
+    count++;
+    year_count++;
+  }
+
+  fprintf(
+    fp,
+    "      </table>\n"
+    "    </div>\n"
+  );
+
+  end_html(fp);
+}
+
+void comp2json(FILE *fp, comp comp) {
+  if (comp.text) {
+    fprintf(
+      fp,
+      "  \"%s\": {\n"
+      "    \"url\": \"%s\",\n"
+      "    \"info\": \"%s\"\n"
+      "  }",
+      comp.name,
+      comp.url,
+      comp.text
+    );
+  }
+
+  else {
+    fprintf(
+      fp,
+      "  \"%s\": {\n"
+      "    \"url\": \"%s\"\n"
+      "  }",
+      comp.name,
+      comp.url
+    );
+  }
+}
+
+void comps2json(FILE *fp) {
+  fprintf(
+    fp,
+    "{\n"
+  );
+
+  comp2json(fp, *comps.items);
+
+  for (size_t i = 1; i < comps.count; i++) {
+    fprintf(
+      fp,
+      ",\n"
+    );
+
+    comp2json(fp, comps.items[i]);
+  }
+
+  fprintf(
+    fp,
+    "\n}"
+  );
+}
+
+int main(void) {
+#include "pbs.c"
+#include "comps.c"
+
+  FILE *fp = fopen(OUTPUT_DIR "/pbs/index.html", "w");
+
+  pbs2html(fp);
+
+  fp = freopen(OUTPUT_DIR "/pbs.json", "w", fp);
+
+  pbs2json(fp);
+
+  fp = freopen(OUTPUT_DIR "/comps/index.html", "w", fp);
+
+  comps2html(fp);
+
+  fp = freopen(OUTPUT_DIR "/comps.json", "w", fp);
+
+  comps2json(fp);
+
+  fclose(fp);
+
+  if (comps.count) free(comps.items);
+}
