ae60ebe7f9cf3593e4fdda18f46ddca18fe16056
Bella Campbell <bella.campbell@tuta.io>
2026-05-18 12:44:12 +0100

    woag :3 Generated By csTimer on 2026-05-18 single: 2:52.49
    Time List:
    1. 2:52.49   3Dw2 B2 U2 Bw2 3Lw' U' B2 Fw2 U' Rw 3Rw' 3Dw L' Bw Fw' 3Fw Dw 3Rw 3Bw' 3Uw' Dw 3Dw U 3Rw2 U2 Uw' 3Uw' Lw2 3Rw2 3Bw2 3Lw2 D2 U2 B' 3Fw2 Fw 3Uw 3Dw2 3Bw Lw2 3Fw' 3Dw D 3Uw 3Rw Fw Rw Uw' F2 Bw U2 F2 Fw Dw' U2 3Uw Fw B Lw2 L' R D2 Uw Fw R' 3Bw' 3Lw2 3Rw' L Fw 3Lw2 B2 Uw' Dw' 3Lw 3Uw2 L2 3Dw' 3Uw2 3Fw Rw' Bw2 Rw B' Fw' 3Lw' Rw 3Uw' 3Lw' 3Uw' Fw' B 3Fw Uw2 Bw Rw2 F Dw D' 3Fw

--- src/pbs.c
+++ src/pbs.c
@@ -8,7 +8,7 @@ PB(e_5x5, single, "48.93");
 PB(e_5x5, average, "57.64");
 PB(e_6x6, single, "1:43.64");
 PB(e_6x6, average, "1:55.00");
-PB(e_7x7, single, "2:53.77");
+PB(e_7x7, single, "2:52.49");
 PB(e_7x7, average, "3:05.14");
 PB(e_3BLD, single, "54.94");
 PB(e_3BLD, average, "1:17.62");
